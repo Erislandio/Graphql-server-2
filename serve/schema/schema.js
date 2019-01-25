7f509fbdcdb87162e68a1bebeb968163b79127c2
Erislandio Soares <erislandiosoares21@gmail.com>
2019-01-25 21:56:13 -0200

    Adição da query root

--- serve/schema/schema.js
+++ serve/schema/schema.js
@@ -1,9 +1,21 @@
 const graphql = require('graphql')
+const _ = require('lodash')
 const {
     GraphQLObjectType,
     GraphQLString,
-    GraphQLInt
+    GraphQLInt,
+    GraphQLSchema
 } = graphql
+
+const users = [
+    {
+        id: '23', fisrtName: "Erislandio", age: 23
+    },
+    {
+        id: '47', fisrtName: "jéssica", age: 18
+    }
+]
+
 
 const UserType = new GraphQLObjectType({
     name: 'User',
@@ -12,4 +24,29 @@ const UserType = new GraphQLObjectType({
         fisrtName: { type: GraphQLString },
         age: { type: GraphQLInt }
     }
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+        user: {
+            type: UserType,
+            args: {
+                id: {
+                    type: {
+                        GraphQLString
+                    }
+                }
+            },
+            resolve(parentValue, args) {
+                return _.find(users, {
+                    id: args.id
+                })
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 })
