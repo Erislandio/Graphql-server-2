661042c7eaef455741b5ff43fad959d787ff520a
Erislandio Soares <erislandiosoares21@gmail.com>
2019-01-28 22:13:36 -0200

    Adição da primieta mutation

--- serve/schema/schema.js
+++ serve/schema/schema.js
@@ -4,16 +4,24 @@ const {
    GraphQLObjectType,
    GraphQLString,
    GraphQLInt,
-   GraphQLSchema
+   GraphQLSchema,
+   GraphQLList,
+   GraphQLNonNull
 } = graphql
 
 const CompanyType = new GraphQLObjectType({
    name: "Company",
-   fields: {
+   fields: () => ({
       id: { type: GraphQLString },
       name: { type: GraphQLString },
-      description: { type: GraphQLString }
-   }
+      description: { type: GraphQLString },
+      users: {
+         type: new GraphQLList(UserType),
+         resolve(parentValue, args) {
+            return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`).then(res => res.data)
+         }
+      }
+   })
 })
 
 const UserType = new GraphQLObjectType({
@@ -54,6 +62,24 @@ const RootQuery = new GraphQLObjectType({
    }
 });
 
+const mutation = new GraphQLObjectType({
+   name: "Mutation",
+   fields: {
+      addUser: {
+         type: UserType,
+         args: {
+            fisrtName: { type: new GraphQLNonNull(GraphQLString) },
+            age: { type: new GraphQLNonNull(GraphQLInt) },
+            companyId: { type: GraphQLString }
+         },
+         resolve(parentValue, { fisrtName, age }) {
+            return axios.post(`http://localhost:3000/users`, { fisrtName, age }).then(res => res.data)
+         }
+      }
+   }
+})
+
 module.exports = new GraphQLSchema({
-   query: RootQuery
+   query: RootQuery,
+   mutation: mutation
 })
